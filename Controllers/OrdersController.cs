d931f61563bff7c403a9f71f89efd8549ee73602
Chaitrali Tambe <136223683+Chaitrali-tambe@users.noreply.github.com>
2026-04-16 13:25:07 +0530

    Add files via upload

---
+++ Controllers/OrdersController.cs
@@ -0,0 +1,42 @@
+﻿using ChaitraliAPI.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.FileProviders;
+using System.Text.Json;
+using System.Linq;
+
+namespace ChaitraliAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrdersController : ControllerBase
+    {
+        private readonly IWebHostEnvironment _env;
+
+        public OrdersController(IWebHostEnvironment env)
+        {
+            _env = env;
+        }
+
+        [HttpGet("getAllOrders")]
+        public async Task<IActionResult> GetAllOrders()
+        {
+            var filepath = Path.Combine(_env.ContentRootPath, "Json", "orders.json");
+
+            if (!System.IO.File.Exists(filepath))
+                return NotFound();
+
+            string jsonstring = await System.IO.File.ReadAllTextAsync(filepath);
+
+            var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true};
+
+            var data = JsonSerializer.Deserialize<OrdersModel>(jsonstring, options);
+
+            var orderList = data?.Orders;
+
+            return Ok(orderList);
+        }
+    }
+
+
+}
