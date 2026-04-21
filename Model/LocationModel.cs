87fad0fd8d5cd54c0ec12d0425145201d2f4a19b
Chaitrali Tambe <136223683+Chaitrali-tambe@users.noreply.github.com>
2026-04-21 16:00:37 +0530

    Add files via upload

---
+++ Model/LocationModel.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ChaitraliAPI.Model
+{
+    public class LocationModel
+    {
+        [JsonPropertyName("locations")]
+        public List<Location>? Location { get; set; }
+    }
+
+    public class Location
+    {
+        public string? Country_Region { get; set; }
+        public string? City { get; set; }
+        public string? State_Province { get; set; }
+        public int Postal_Code { get; set; }
+        public string? Region { get; set; }
+    }
+}
---
+++ Model/TransactionModel.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ChaitraliAPI.Model
+{
+    public class TransactionModel
+    {
+        [JsonPropertyName("locations")]
+        public List<Transaction>? Transaction { get; set; }
+        
+    }
+
+    public class Transaction
+    {
+        public string? Order_ID { get; set; }
+        public string? Product_ID { get; set; }
+        public string? Sales { get; set; }
+        public string? Quantity { get; set; }
+        public string? Discount { get; set; }
+        public string? Profit { get; set; }
+    }
+
+}
